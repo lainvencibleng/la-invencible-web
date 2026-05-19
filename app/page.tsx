dc77860f911b1441808215fda2c1288dd0da8588
La Invencible <lainvencibleng@gmail.com>
2026-05-18 21:43:09 -0300

    cambio texto

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function LaInvencibleNG() {
 
             <div>
               <h1 className="text-2xl font-bold tracking-tight text-slate-900">
-                pepito vacas
+                La Invencible
               </h1>
               <p className="text-sm text-slate-500 tracking-wide uppercase">
                 Negocios Ganaderos
